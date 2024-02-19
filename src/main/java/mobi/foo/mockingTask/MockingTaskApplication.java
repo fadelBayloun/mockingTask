f39eaabca06145d632522e2f298b4ae681647d8a
fadelBayloun <fadel.bayloun12@gmail.com>
2024-02-19 10:42:31 +0200

    initial commit

---
+++ src/main/java/mobi/foo/mockingTask/MockingTaskApplication.java
@@ -0,0 +1,13 @@
+package mobi.foo.mockingTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockingTaskApplication.class, args);
+	}
+
+}
